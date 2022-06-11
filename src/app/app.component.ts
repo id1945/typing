e5c879675c444dc0a0143f70f1897da03870372d
daidh1 <daidh@goline.vn>
2022-06-11 23:21:11 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+interface Typing {
+  value: string;
+  active: boolean;
+  status: string;
+}
+
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None,
@@ -10,8 +16,7 @@ export class AppComponent implements OnInit {
   /**
    * Declare variable
    */
-  public typing = [];
-  public data: { value: string; active: boolean; status: string }[] = [];
+  public data: Typing[] = [];
   public text =
     'We and our partners store and/or access information on a device, such as cookies and process personal data, such as unique identifiers and standard information sent by a device for personalised ads and content, ad and content measurement, and audience insights, as well as to develop and improve products.With your permission we and our partners may use precise geolocation data and identification through device scanning. You may click to consent to our and our partners’ processing as described above. Alternatively you may access more detailed information and change your preferences before consenting or to refuse consenting.Please note that some processing of your personal data may not require your consent, but you have a right to object to such processing. Your preferences will apply to this website only. You can change your preferences at any time by returning to this site or visit our privacy policy.';
 
@@ -25,9 +30,9 @@ export class AppComponent implements OnInit {
      */
     const strArr = this.shuffleArray(this.text.split(' '));
     /**
-     *
+     * Active
      */
-    this.data = strArr.map((m, i) => ({
+    this.data = strArr.map((m: string, i: Number) => ({
       value: m,
       active: i == 0 ? true : false,
       status: i == 0 ? 'label-default' : '',
@@ -36,30 +41,17 @@ export class AppComponent implements OnInit {
 
   /**
    * Space event
+   * @param e 
    */
-  onSpace(e) {
-
-    /**
-     * Data map
-     */
+  onSpace(e: any) {
     this.data = this.data.map((m, i) => {
       const value = e?.target?.value?.trim();
       const previous = this.data[i != 0 ? i - 1 : 0];
-      const equal = this.replace(m.value) == value;
+      const equal = m.value == value;
       if (m.status) {
-        /**
-         * text-success
-         * text-danger
-         */
-        if (equal) {
-          return { ...m, active: false, status: 'text-success'};
-        } else {
-          return { ...m, active: false, status: new RegExp(/(default|success)/g).test(m.status) ? 'text-success' : 'text-danger'};
-        }
+        const status = equal ? 'text-success' : (new RegExp(/(default|success)/g).test(m.status) ? 'text-success' : 'text-danger');
+        return { ...m, active: false, status: status };
       } if (previous.status) {
-        /**
-         * label-default
-         */
         return { ...m, active: true, status: 'label-default' };
       } else {
         return m;
@@ -68,20 +60,21 @@ export class AppComponent implements OnInit {
 
     // Clean
     e.target.value = '';
+
+    // audio play
+    this.playAudioJS('typing/assets/bombom.mp3', 1);
   }
 
   /**
-   * Input
+   * onInput
+   * label-default
+   * label-danger
    */
-  onInput(e) {
+  onInput(e: any) {
     if (!e.Space) {
-      /**
-       * label-default
-       * label-danger
-       */
       this.data = this.data.map((m, i) => {
         const value = e?.target?.value?.trim();
-        const equal = m?.value?.match(value);
+        const equal = m?.value === value;
         if (m.active) {
           return { ...m, status: equal ? 'label-default' : 'label-danger' };
         } else {
@@ -91,7 +84,12 @@ export class AppComponent implements OnInit {
     }
   }
 
-  shuffleArray(array) {
+  /**
+   * shuffleArray
+   * @param array 
+   * @returns 
+   */
+  shuffleArray(array: string[]) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
@@ -100,14 +98,52 @@ export class AppComponent implements OnInit {
   }
 
   /**
-   * Remove special character
+   * trackByFn
+   * @param index 
+   * @param item 
+   * @returns 
    */
-  replace(text) {
-    return text.replace(/([,.;:])/g, '');
-  }
-
-  trackByFn(index, item) {    
+  trackByFn(index: Number, item: any) {
     return item.id; // unique id corresponding to the item
   }
 
+  /**
+   * @param {*} path: audio
+   * @param {*} volume: number
+   */
+  playAudioJS(path: string, volume: number) {
+    let audio = new Audio();
+    audio.src = path;
+    audio.volume = volume;
+    audio.load();
+    audio.play().catch(function () {
+      console.log('Exception play audio: issue');
+    });
+    return audio;
+  }
+
+  /**
+   * @param {*} path: src audio
+   * @param {*} volume: number
+   */
+  playAudioHtmlRestart(path: string, volume: number) {
+    // let audio = ...; // Declare a local variable
+    // audio.currentTime = 0;
+    // audio.volume = volume;
+    // audio.src = path;
+    // audio.play().catch(function () {
+    //   console.log('Exception play audio: issue');
+    // });
+    // return audio;
+  }
+
+  /**
+   * Stop audio html
+   */
+  stopAudioHtml() {
+    // let audio = ...; // Declare a local variable
+    // audio.src = '';
+    // audio.currentTime = 0;
+    // return audio;
+  }
 }
