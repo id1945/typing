cc41eb9f951b5e0e98cc1dae2407cb3f7648354c
daidh1 <daidh@goline.vn>
2022-06-13 22:35:41 +0700

    Storage data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,30 +19,33 @@ export class AppComponent implements OnInit {
   /**
    * Declare variable
    */
-  public data: Typing[] = [];
-  public text =
-    'We and our partners store and/or access information on a device, such as cookies and process personal data, such as unique identifiers and standard information sent by a device for personalised ads and content, ad and content measurement, and audience insights, as well as to develop and improve products.With your permission we and our partners may use precise geolocation data and identification through device scanning. You may click to consent to our and our partners’ processing as described above. Alternatively you may access more detailed information and change your preferences before consenting or to refuse consenting.Please note that some processing of your personal data may not require your consent, but you have a right to object to such processing. Your preferences will apply to this website only. You can change your preferences at any time by returning to this site or visit our privacy policy.';
+  public dataMap: Typing[] = [];
+  public dataText: string = 'We and our partners store and/or access information on a device, such as cookies and process personal data, such as unique identifiers and standard information sent by a device for personalised ads and content, ad and content measurement, and audience insights, as well as to develop and improve products. With your permission we and our partners may use precise geolocation data and identification through device scanning. You may click to consent to our and our partners’ processing as described above. Alternatively you may access more detailed information and change your preferences before consenting or to refuse consenting. Please note that some processing of your personal data may not require your consent, but you have a right to object to such processing. Your preferences will apply to this website only. You can change your preferences at any time by returning to this site or visit our privacy policy.';
 
   /**
    * Init lifecycle
    */
   ngOnInit(): void {
+    // load store
+    this.initStore();
     // init data
     this.initData();
-  }
-  
-  initData() {
     // play welcome
     this.playAudioJS(`assets/welcome.mp3?v=${Date.now()}`, 1);
-    /**
-     * Convert text to array
-     * Random item
-     */
-    const strArr = this.shuffleArray(this.text.split(' '));
-    /**
-     * Active
-     */
-    this.data = strArr.map((m: string, i: Number) => ({
+  }
+
+  /**
+   * initStore
+   */
+  initStore() {
+    this.dataText = localStorage.getItem('typing-data') ?? this.dataText;
+  }
+
+  /**
+   * initData
+   */
+  initData(dataMap = this.dataText?.split(' ')) {
+    this.dataMap = dataMap.map((m: string, i: Number) => ({
       value: m,
       active: i == 0 ? true : false,
       status: i == 0 ? 'label-default' : '',
@@ -54,9 +57,9 @@ export class AppComponent implements OnInit {
    * @param e 
    */
   onSpace(e: any) {
-    this.data = this.data.map((m, i) => {
+    this.dataMap = this.dataMap.map((m, i) => {
       const value = e?.target?.value?.trim();
-      const previous = this.data[i != 0 ? i - 1 : 0];
+      const previous = this.dataMap[i != 0 ? i - 1 : 0];
       const equal = m.value == value;
       if (m.status) {
         const status = equal ? 'text-success' : (new RegExp(/(default|success)/g).test(m.status) ? 'text-success' : 'text-danger');
@@ -69,9 +72,9 @@ export class AppComponent implements OnInit {
     });
 
     // audio play
-    for (const [i, item] of this.data.entries()) {
+    for (const [i, item] of this.dataMap.entries()) {
       const value = e?.target?.value?.trim();
-      const previous = this.data[i != 0 ? i - 1 : 0];
+      const previous = this.dataMap[i != 0 ? i - 1 : 0];
       const equal = previous?.value === value;
       if (item.active) {
         this.playAudioJS(`assets/${equal ? 'success' : 'wrong'}.mp3`, 1);
@@ -89,7 +92,7 @@ export class AppComponent implements OnInit {
    */
   onInput(e: any) {
     if (!e.Space) {
-      this.data = this.data.map((m, i) => {
+      this.dataMap = this.dataMap.map((m, i) => {
         const value = e?.target?.value?.trim();
         const equal = m?.value === value;
         if (m.active) {
@@ -99,6 +102,28 @@ export class AppComponent implements OnInit {
         }
       });
     }
+  }
+
+  /**
+   * onRandom
+   */
+  onRandom() {
+    this.initData(this.shuffleArray(this.dataText?.split(' ')));
+    this.playAudioJS(`assets/random.mp3?v=${Date.now()}`, 1);
+  }
+
+  /**
+   * onUpdate
+   */
+  onUpdate() {
+    // store
+    localStorage.setItem('typing-data', this.dataText);
+    // load store
+    this.initStore();
+    // init data
+    this.initData();
+    // play update-success
+    this.playAudioJS(`assets/update-success.mp3?v=${Date.now()}`, 1);
   }
 
   /**
@@ -131,36 +156,10 @@ export class AppComponent implements OnInit {
   playAudioJS(path: string, volume: number) {
     const audio = new Audio();
     audio.currentTime = 0;
-    audio.autoplay = true;
     audio.src = path;
     audio.volume = volume;
-    audio.load();
     audio.play().catch((error) => {
       console.log('Exception play audio: issue', error);
     });
-  }
-
-  /**
-   * @param {*} path: src audio
-   * @param {*} volume: number
-   */
-  playAudioHtmlRestart(path: string, volume: number) {
-    // const audio = this.audioRef.nativeElement as HTMLAudioElement; // Declare a local variable
-    // audio.currentTime = 0;
-    // audio.volume = volume;
-    // audio.src = path;
-    // audio.load();
-    // audio.play().catch((error) => {
-    //   console.log('Exception play audio: issue', error);
-    // });
-  }
-
-  /**
-   * Stop audio html
-   */
-  stopAudioHtml() {
-    // const audio = ...; // Declare a local variable
-    // audio.src = '';
-    // audio.currentTime = 0;
   }
 }
